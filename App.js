e2169218ed6b5c6ec0d94bb16f3e88a300f38807
Sabinkumar <sabinthe5@gmail.com>
2025-11-27 23:34:41 +0000

    React component basics

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-const parent  = React.createElement('div',{id:"parent"},[
-    React.createElement('div',{id:'child'},
-        [React.createElement('h1',{},"Im h1 tag"),
-            React.createElement('h2',{},"Im h2 tag")]
-    ),
-    React.createElement('div',{id:"child2"},
-        [React.createElement('h1',{},"Im h2 tag"),
-            React.createElement('h2',{},"Im h2 tag")]
-    )
-])
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Sabin Dev is Here"
+);
+const jsxHeading = (
+  <h1 id="heading" tabIndex="5">
+    Sabin Dev using JSX
+  </h1>
+);
+const Title = () => <h1 className="head">This is Title component</h1>;
+const HeadingComponent = () => (
+  <div id="container">
+    {jsxHeading}
+    {Title()}
+    <Title />
+    <Title></Title>
+
+    <h1>This is a React functional component </h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent />);
